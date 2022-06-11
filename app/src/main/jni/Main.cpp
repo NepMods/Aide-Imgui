61cd6eaea3f4a17ec34f33d5e95c8332716c3863
NepMods <88999135+NepMods@users.noreply.github.com>
2022-06-11 17:39:04 +0545

    Removed Useless Codes

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -1,3 +1,58 @@
+#define targetLibName OBFUSCATE("libil2cpp.so")
+
+uintptr_t Touch = 0xE2C5A8;
+
+// pulic class Input
+
+// public static Touch GetTouch(int index) { }
+
+struct Patches {
+
+    MemoryPatch MemoryExample;
+
+} hexPatches;
+
+struct Variables {
+
+    bool BooleanExample = false;
+
+} FT;
+
+void DrawImGui() {
+
+   
+
+    
+
+    ImGui::NewFrame();
+
+    // Render ImGui windows here.
+
+    ImGui::ShowDemoWindow();
+
+    
+
+    ImGui::EndFrame();
+
+    ImGui::Render();
+
+}
+
+void *hack_thread(void *) {
+
+    LOGI(OBFUSCATE("pthread created"));
+
+    sleep(5);
+
+    do {
+
+        sleep(1);
+
+    } while (!isLibraryLoaded(targetLibName));
+
+    
+
+}
 uintptr_t Touch = 0xE2C5A8;
 // pulic class Input
 // public static Touch GetTouch(int index) { }
